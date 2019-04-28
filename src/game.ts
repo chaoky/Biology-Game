18e1f613a15a03b713a71a4ea3478a578dc61840
lordie <levimanga@gmail.com>
2019-04-28 05:47:51 -0300

    fixed prize bug

--- src/game.ts
+++ src/game.ts
@@ -54,7 +54,7 @@ class Board extends VuexModule {
       question: "Eu cataliso a fermentação láctica. Quem sou eu?",
       options: ["Lactatonase", "Lactato-desidrogenase"],
       answer: "Lactato-desidrogenase",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
@@ -62,14 +62,14 @@ class Board extends VuexModule {
         "Sou uma das enzimas do ponto de controle da glicólise. Quem sou eu?",
       options: ["Fosfofrutoquinase", "Hexolatoquinase"],
       answer: "Fosfofrutoquinase",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
       question: "Eu sou a primeira via metabólica elucidada. Quem sou eu?",
       options: ["Glicólise", "Ciclo da ureia"],
       answer: "Glicólise",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
@@ -77,7 +77,7 @@ class Board extends VuexModule {
         "No ciclo do ácido eu sou responsável pela formação de citrato e tenho feedback negativo. Quem sou eu?",
       options: ["Etapa 5", "Etapa 1"],
       answer: "Etapa 1",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
@@ -85,7 +85,7 @@ class Board extends VuexModule {
         "Somos inibidores do complexo de -cetoglutarato no ciclo do ácido cítrico. Quem sou eu?",
       options: ["ATP, NADH e succinilCoA", "ATP, NADH e O2"],
       answer: "ATP, NADH e succinilCoA",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
@@ -96,14 +96,14 @@ class Board extends VuexModule {
         "Complexo da piruvato-desidrogenase"
       ],
       answer: "Complexo da piruvato-desidrogenase",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
       question: "Na glicólise sou a fase de gasto de ATP. Quem sou eu?",
       options: ["Fase de liberação", "Fase preparatória"],
       answer: "Fase preparatória",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
@@ -111,7 +111,7 @@ class Board extends VuexModule {
         "Na ausência do oxigênio na glicólise dessas oxidações não acontecem. Quem sou eu?",
       options: ["NADH e piruvato", "FADH e C"],
       answer: "NADH e piruvato",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
@@ -119,7 +119,7 @@ class Board extends VuexModule {
         "Em eucariotos o ciclo ácido cítrico ocorre em mim. Quem sou eu?",
       options: ["Mitocôndria", "Núcleo"],
       answer: "Mitocôndria",
-      prize: 1
+      prize: 0
     },
     {
       color: "green",
@@ -130,7 +130,7 @@ class Board extends VuexModule {
         "Tirosina, estrogênio e progesterona"
       ],
       answer: "Glucagon, adrenalina e insulina",
-      prize: 1
+      prize: 0
     }
   ];
   omega: Array<Card> = [
@@ -170,7 +170,7 @@ class Board extends VuexModule {
         "-A glicolise é a primeira via metabólica da glicose e apresenta dez reações quimicas. Esse importante processo ocorre no interior da célula, mais precisamente:",
       options: ["Mitocôndria", "Citosol", "Núcleo"],
       answer: "Citosol",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -181,7 +181,7 @@ class Board extends VuexModule {
         "Liberação de energia para as funções vitais celulares"
       ],
       answer: "Liberação de energia para as funções vitais celulares",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -193,7 +193,7 @@ class Board extends VuexModule {
         "Fosfoglicerato-quinase"
       ],
       answer: "Fosfogliceromutase",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -201,7 +201,7 @@ class Board extends VuexModule {
         "Após duas voltas no ciclo do ácido cítrico, observa-se que os carbonos derivados da glicose são oxidados e liberados como moléculas de:",
       options: ["O2", "H2O", "CO2"],
       answer: "CO2",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -213,7 +213,7 @@ class Board extends VuexModule {
         "3-fosfoglicerato"
       ],
       answer: "Frutose-6-fosfato",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -221,7 +221,7 @@ class Board extends VuexModule {
         "Qual das alternativas é um ponto de controle da via glicolítica?",
       options: ["Hexquinase", "Fosfotrutoquinastase", "Piruvato-neostase"],
       answer: "Hexquinase",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -229,7 +229,7 @@ class Board extends VuexModule {
         "A glicólise é uma via metabólica que tem por objetivo oxidar a glicose a fim de conseguir ATP. Nesse processo, a glicose é convertida em duas moléculas de:",
       options: ["Aminoácidos", "Piruvato", "Oxilacetato"],
       answer: "Piruvato",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -237,7 +237,7 @@ class Board extends VuexModule {
         "No ciclo de Krebs, observa-se por ciclo a produção de quantas moléculas de ATP?",
       options: ["1", "2", "3"],
       answer: "1",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -245,7 +245,7 @@ class Board extends VuexModule {
         "O ciclo de Krebs é uma das etapas de um importante processo que ocorre no organismo de certo seres vivos. Esse processo, que está relacionado com a produção de energia para a célula, é chamado de:",
       options: ["Fotossíntese", "Fermentação alcoólica", "Respiração celular"],
       answer: "Respiração celular",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -257,28 +257,28 @@ class Board extends VuexModule {
         "Em três moléculas de ácido pirúvico"
       ],
       answer: "Em duas moléculas de ácido pirúvico",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
       question: "Quantos carbonos tem a molécula de glicose?",
       options: ["12", "6", "3"],
       answer: "6",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
       question: "Qual é o saldo final de ATP no processo de glicólise?",
       options: ["2", "1", "6"],
       answer: "2",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
       question: "A glicólise é dividida em quantas fases?",
       options: ["2", "4", "3"],
       answer: "2",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -286,7 +286,7 @@ class Board extends VuexModule {
         "É correto afirmar que a glicólise é o processo responsável pela quebra da glicose, transformando-a em piruvato ou ácido pirúvico?",
       options: ["Sim", "Não"],
       answer: "Sim",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -298,14 +298,14 @@ class Board extends VuexModule {
         "Duas molécula de ATP e forma quatro"
       ],
       answer: "Duas molécula de ATP e forma quatro",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
       question: "Existem quantos pontos de controle no ciclo do ácido cítrico?",
       options: ["3", "4", "5"],
       answer: "3",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -317,14 +317,14 @@ class Board extends VuexModule {
         "Cadeia transportadora de elétrons"
       ],
       answer: "Ciclo do ácido cítrico do catabolismo",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
       question: "Onde ocorre a glicólise?",
       options: ["Matriz mitocondrial", "Citosol das células", "Núcleo celular"],
       answer: "Citosol das células",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -335,7 +335,7 @@ class Board extends VuexModule {
         "Segunda reação da fase preparatória"
       ],
       answer: "Terceira reação da fase preparatória",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -346,7 +346,7 @@ class Board extends VuexModule {
         "8 reações, apenas 2 na matriz mitocondrial"
       ],
       answer: "8 reações, apenas 2 na matriz mitocondrial",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -358,7 +358,7 @@ class Board extends VuexModule {
       ],
       answer:
         "O ciclo de Krebs inicia-se com a reação entre acetil-CoA e ácido oxalacético",
-      prize: 1
+      prize: 0
     },
     {
       color: "red",
@@ -370,7 +370,7 @@ class Board extends VuexModule {
       ],
       answer:
         "Ocorre nas três etapas principais: glicólise, ciclo de Krebs e cadeia transportadora de elétrons",
-      prize: 1
+      prize: 0
     }
   ];
   yellow: Array<Card> = [
@@ -380,7 +380,7 @@ class Board extends VuexModule {
         "O complexo da piruvato-desidrogenase não é inibido por ATP e NADH.",
       options: ["Verdadeiro", "Falso"],
       answer: "Falso",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
@@ -388,7 +388,7 @@ class Board extends VuexModule {
         "No ciclo do ácido cítrico o citrato é isomerizado em policitrato.",
       options: ["Verdadeiro", "Falso"],
       answer: "Falso",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
@@ -396,7 +396,7 @@ class Board extends VuexModule {
         "A glicólise tem um saldo líquido de 4 ATP, mas se acoplada a outras vias pode chegar a 100. ",
       options: ["Verdadeiro", "Falso"],
       answer: "Falso",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
@@ -404,21 +404,21 @@ class Board extends VuexModule {
         "Na etapa preparatória que existe fora do ciclo do ácido cítrico acontece a conversão de piruvato a citrato.",
       options: ["Verdadeiro", "Falso"],
       answer: "Falso",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
       question: "Existem 5 pontos de controle no cíclo do ácido cítrico.",
       options: ["Verdadeiro", "Falso"],
       answer: "Falso",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
       question: "Glicose é armazenada como amido e glicogênio.",
       options: ["Verdadeiro", "Falso"],
       answer: "Verdadeiro",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
@@ -426,7 +426,7 @@ class Board extends VuexModule {
         "Na via glicolítica a molécula de glicose (6C) é degradada por uma sequência de 10 reações e 2 moléculas de piruvato (3C).",
       options: ["Verdadeiro", "Falso"],
       answer: "Verdadeiro",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
@@ -434,14 +434,14 @@ class Board extends VuexModule {
         "No ciclo do ácido cítrico a conversão de piruvato a acetil-CoA se dá devido ao complexo da piruvato-desidrogenase.",
       options: ["Verdadeiro", "Falso"],
       answer: "Verdadeiro",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
       question: "O ciclo do ácido cítrico é altamente exergônico.",
       options: ["Verdadeiro", "Falso"],
       answer: "Verdadeiro",
-      prize: 1
+      prize: 0
     },
     {
       color: "yellow",
@@ -449,7 +449,7 @@ class Board extends VuexModule {
         "A glicólise anaeróbica é o processo responsável pela quebra da glicose, transformando-a em piruvato ou ácido pirúvico.",
       options: ["Verdadeiro", "Falso"],
       answer: "Verdadeiro",
-      prize: 1
+      prize: 0
     }
   ];
 
@@ -461,7 +461,7 @@ class Board extends VuexModule {
     question: "",
     options: [],
     answer: "",
-    prize: 1
+    prize: 0
   };
   awnser: boolean = true;
 
@@ -485,7 +485,7 @@ class Board extends VuexModule {
         question: "Acabaram as cartas desta cor",
         options: ["O paulo = merda???"],
         answer: "O paulo = merda???",
-        prize: 1
+        prize: 0
       };
     }
   }
